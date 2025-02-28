364d3587de78aabc8e040d5c88faf54fa409cf6e
UyVo <uyvo0408@gmail.com>
2025-02-28 23:26:52 +0700

    fix: errol weather_forecast Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/router/app_router.dart';
+import 'package:weather_app/screen/weather_forecast/bloc/weather_forecast_bloc.dart';
 
 import 'app_bloc/app_bloc.dart';
 
@@ -14,8 +15,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AppBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AppBloc()),
+        BlocProvider(create: (context) => WeatherForecastBloc()),
+      ],
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
         routerConfig: AppRouter().router,
