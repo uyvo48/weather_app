56910ac4bb3260efbf4c2e5a795410aedd9950d9
uyvo48 <uyvo0408@gmail.com>
2025-04-21 09:29:47 +0700

    add: fix init location home screen

--- lib/screen/themes/controller/theme_controller.dart
+++ lib/screen/themes/controller/theme_controller.dart
@@ -2,14 +2,6 @@ import '../../../util/images.dart';
 
 class ThemeController {
   List<String> listImageTheme = [
-    // ThemeItem(imageTheme1),
-    // ThemeItem(imageTheme2),
-    // ThemeItem(imageTheme3),
-    // ThemeItem(imageTheme4),
-    // ThemeItem(imageTheme5),
-    // ThemeItem(imageTheme6),
-    // ThemeItem(imageTheme7),
-    // ThemeItem(imageTheme8),
     imageTheme1,
     imageTheme2,
     imageTheme3,
